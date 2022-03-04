1bd92ba63ecc2be8b98eb5a48f95af8b8944bbd6
jonmallozzi <jonathan.mallozzi@maine.edu>
2022-03-04 16:00:31 -0500

    Started the Sevelte Site and Project

---
+++ site/portfolio/src/App.svelte
@@ -0,0 +1,47 @@
+<main>
+  <h1>Jon Mallozzi</h1>
+  <h3>A Full Stack Software Engineer, Game Dev and a Big Dreamer</h3>
+
+  <p>
+    Check out my <a href="https://github.com/JonMallozzi">github</a>  for more info
+  </p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
